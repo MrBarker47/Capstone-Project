068b60c5f8f7bbdd5ef76a42da2f8254fdfdc59e
MrBarker47 <jebarker7@gmail.com>
2024-12-16 13:01:12 -0500

    Added a card element to my service using react-bootstrap

--- capstone-project/src/pages/Service.jsx
+++ capstone-project/src/pages/Service.jsx
@@ -2,9 +2,10 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
+
 export const Service = function() {
     return(
-        <div>
+        <div className='Center'>
         <Card className="Card-Background" style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
