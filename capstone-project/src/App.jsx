d74a6f4993901c9a4d98d49559474e1f04a102aa
MrBarker47 <jebarker7@gmail.com>
2024-12-16 12:55:07 -0500

    Update files

--- capstone-project/src/App.jsx
+++ capstone-project/src/App.jsx
@@ -2,7 +2,9 @@ import {Routes, Route} from 'react-router-dom'
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {Contact} from "./pages/Contact"
+import {Service} from "./pages/Service"
 import {Navbar} from "./Links/Navbar"
+
 
 
 import './App.css'
@@ -15,6 +17,7 @@ function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/About" element={<About/>}/>
       <Route path="/Contact" element={<Contact />} />
+      <Route path="/Service" element={<Service />} />
   </Routes>
   </>
   )
--- capstone-project/src/pages/Service.jsx
+++ capstone-project/src/pages/Service.jsx
@@ -1,9 +1,45 @@
-import './App.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 export const Service = function() {
     return(
         <div>
-            
+        <Card className="Card-Background" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the cards content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
+        <br />
+         <Card className="Card-Background" style={{ width: '18rem' }}>
+         <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the cards content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
+        <br />
+         <Card className="Card-Background" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+           <Card.Body>
+           <Card.Title>Card Title</Card.Title>
+           <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the cards content.
+          </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
         </div>
     )
 }
