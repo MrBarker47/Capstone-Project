2b239223fe96003ddf94eddd2489c760e26cc305
MrBarker47 <jebarker7@gmail.com>
2024-12-16 11:38:46 -0500

    Update files

--- capstone-project/src/pages/Service.jsx
+++ capstone-project/src/pages/Service.jsx
@@ -1,19 +1,9 @@
 import './App.css'
 
-function Service() {
+export const Service = function() {
     return(
         <div>
-       <nav>
-       <img src="/images/BBQ.png" alt="steak on a the grill" style={{height:150, width:140}}/>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">Service</a></li>
-        </ul>
-       </nav>
+            
         </div>
     )
 }
-
-export default Service;
