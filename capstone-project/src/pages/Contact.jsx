d77e342fc14e8856bc85a5b4e82f6032baf22d66
MrBarker47 <jebarker7@gmail.com>
2024-12-16 15:47:46 -0500

    Update the contact page and added a form

--- capstone-project/src/pages/Contact.jsx
+++ capstone-project/src/pages/Contact.jsx
@@ -1,15 +1,21 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-
- export const Contact = function() {
-    return(
-        <div>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name"/>
-                </label>
-            </form>
-        </div>
-    )
+export const Contact = function() {
+  return (
+    <Form>
+      <Form.Group className="Form" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control className="Input" type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control className="Input2" as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant='primary' type="submit" >Submit</Button>
+    </Form>
+  );
 }
+
+
 
