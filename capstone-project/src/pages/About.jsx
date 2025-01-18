4ad515f759c219b7f5151afad66ddee49291f6e0
MrBarker47 <jebarker7@gmail.com>
2025-01-18 16:05:38 -0500

    Update files

--- capstone-project/src/pages/About.jsx
+++ capstone-project/src/pages/About.jsx
@@ -3,7 +3,7 @@ export const About = function() {
         <div>
             
             <img className="Grandma" src="/images/Grandma.png" alt="photo of my grandma" style={{width: 220}} />
-            <h1 className="Alabama">The Lady From Alabama</h1>
+            <h1 className="Alabama">The Lady From <h1 className="Alabama2">Alabama</h1></h1>
         <p className="paragraph">Bertha’s BBQ was inspired by my grandma, Bertha Mae Barker. 
             Who sadly passed away, September 13th, 2024. The house I was raised in is hers, over the years we’ve 
             had plenty of BBQs in the backyard. The week, I started my software-engineer program she passed away. When I found out about her passing, that Friday morning. I was extremely hurt behind it. In her honor I wanted to build a website, based on the amount BBQs we had over here. 
