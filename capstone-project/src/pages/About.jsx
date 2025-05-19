0e840ac3ee7f37328be416333d98484b17d8b38f
MrBarker47 <jebarker7@gmail.com>
2025-05-19 10:08:21 -0400

    Update files

--- capstone-project/src/pages/About.jsx
+++ capstone-project/src/pages/About.jsx
@@ -1,14 +1,13 @@
 export const About = function() {
     return(
         <div>
-            
-            <img className="Grandma" src="/images/Grandma.png" alt="photo of my grandma" style={{width: 220}} />
-            <h1 className="Alabama">The Lady From <h1 className="Alabama2">Alabama</h1></h1>
+        <img className="Grandma" src="/images/Grandma.png" alt="photo of my grandma" style={{width: 220}} />
+        <h1 className="Alabama">The Lady From <h1 className="Alabama2">Alabama</h1></h1>
         <p className="paragraph">Bertha’s BBQ was inspired by my grandma, Bertha Mae Barker. 
             Who sadly passed away, September 13th, 2024. The house I was raised in is hers, over the years we’ve 
             had plenty of BBQs in the backyard. The week, I started my software-engineer program she passed away. When I found out about her passing, that Friday morning. I was extremely hurt behind it. In her honor I wanted to build a website, based on the amount BBQs we had over here. 
              And I wanted to name in after her, someone who was caring, loving dedicated to get things done and she would have a smile on her face. That’s what Bertha’s BBQ, is all about and my grandma represented that to the fullest. I love you grandma, thank you for everything. 
-</p>
+        </p>
         </div>
     )
 }
--- capstone-project/src/pages/Contact.jsx
+++ capstone-project/src/pages/Contact.jsx
@@ -8,8 +8,8 @@ export const Contact = function() {
       <Form.Group className="Form" controlId="exampleForm.ControlInput1">
         <Form.Label className="Label">Email address</Form.Label>
         <Form.Control className="Input" type="email" placeholder="name@example.com" required/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+       </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label className='Form-Message'>Message</Form.Label>
         <Form.Control className="Input2" as="textarea" rows={3} required/>
         </Form.Group>
